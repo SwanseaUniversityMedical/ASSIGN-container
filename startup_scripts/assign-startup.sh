6ae73613cff55f7c33bce61397c9712bab50d65d
Mike Edwards <michael.edwards@swansea.ac.uk>
2024-11-14 14:34:25 +0000

    refactor: pretty up the logging of the startup script

--- startup_scripts/assign-startup.sh
+++ startup_scripts/assign-startup.sh
@@ -7,18 +7,21 @@ mkdir -p /data/logs
 
 # Get ASSIGN
 if [ ! -d "$assign_dest" ]; then
+  echo "Obtaining ASSIGN routines."
   # Clone the wanted sha from github
   git clone $assign_url $assign_dest
   git -C $assign_dest checkout $assign_sha
 
   # Put the routines to the YottaDB routines directory
+  echo "Moving the routines."
   cp $assign_dest/UPRN/yottadb/* $ydb_dir/$ydb_rel/r
   cp $assign_dest/UPRN/codelists/* $abp_dir
 
   # Perform zlink of routines
+  echo "Linking ASSIGN routines."
   $ydb_dist/ydb -run ^ZLINK
 else
-  echo "$assign_dest already exists. Not cloning down ASSIGN again"
+  echo "$assign_dest already exists. Not cloning down ASSIGN again."
 fi
 
 # Update YottaDB database settings
