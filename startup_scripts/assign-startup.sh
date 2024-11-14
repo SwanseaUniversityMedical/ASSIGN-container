d01523237392510764bd6dd5c750b015bb70f182
Mike Edwards <michael.edwards@swansea.ac.uk>
2024-11-14 14:28:59 +0000

    feat: git clone now checks for existing destination directory and does not pull again if it exists, but doesn't check if it is populated.

--- startup_scripts/assign-startup.sh
+++ startup_scripts/assign-startup.sh
@@ -2,27 +2,33 @@
 
 mkdir -p /data/logs
 
-#set env vars
+# Set env vars
 . /opt/yottadb/current/ydb_env_set
 
-#get ASSIGN
-git clone $assign_url $assign_dest
-git -C $assign_dest checkout $assign_sha
-cp $assign_dest/UPRN/yottadb/* $ydb_dir/$ydb_rel/r
-mkdir $abp_dir
-cp $assign_dest/UPRN/codelists/* $abp_dir
+# Get ASSIGN
+if [ ! -d "$assign_dest" ]; then
+  # Clone the wanted sha from github
+  git clone $assign_url $assign_dest
+  git -C $assign_dest checkout $assign_sha
 
-#perform zlink of routines
-$ydb_dist/ydb -run ^ZLINK
+  # Put the routines to the YottaDB routines directory
+  cp $assign_dest/UPRN/yottadb/* $ydb_dir/$ydb_rel/r
+  cp $assign_dest/UPRN/codelists/* $abp_dir
 
-# extend database to hold ABP
+  # Perform zlink of routines
+  $ydb_dist/ydb -run ^ZLINK
+else
+  echo "$assign_dest already exists. Not cloning down ASSIGN again"
+fi
+
+# Update YottaDB database settings
 $ydb_dist/mupip set -NULL_SUBSCRIPTS=true -region DEFAULT && \
 $ydb_dist/mupip set -journal=off -region DEFAULT && \
 $ydb_dist/mupip set -extension_count=500000 -region DEFAULT && \
 $ydb_dist/mupip set -access_method=mm -region DEFAULT
 
-#startup webgui
+# Startup webgui
 yottadb -run %ydbgui --read --port 9080 >>/data/logs/%ydbgui.log &
 
-#drop in to ydb
+# Drop in to ydb
 exec $ydb_dist/yottadb -direct
