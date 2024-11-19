f750009d49676d622a72d4606ecbd4784caff22c
Mike Edwards <michael.edwards@swansea.ac.uk>
2024-11-19 11:07:10 +0000

    Add TLS services and testing scripts for endpoint API

--- startup_scripts/assign-startup.sh
+++ startup_scripts/assign-startup.sh
@@ -34,17 +34,17 @@ $ydb_dist/mupip set -access_method=mm -region DEFAULT
 export ydb_tls_passwd_dev="$($ydb_dist/plugin/ydbcrypt/maskpass <<< $cert_pass | cut -d ":" -f2 | tr -d ' ')"
 
 # Startup webgui
-yottadb -run %ydbgui --read --port 9080 >>/data/logs/%ydbgui.log &
+yottadb -run %ydbgui --readwrite --port 9080 >>/data/logs/%ydbgui.log &
 
 # Startup the ASSIGN TLS listener
 yottadb -run %XCMD 'job START^VPRJREQ(9081)' &
 
 # Startup the ASSIGN web API interface
 mv /data/ADDWEBAUTH.m $ydb_dir/$ydb_rel/r/ADDWEBAUTH.m
-yottadb -run INT^VUE
-yottadb -run SETUP^UPRNHOOK2
-yottadb -run ^NEL
-yottadb -run ^ADDWEBAUTH
+yottadb -run INT^VUE          # File upload/download
+yottadb -run SETUP^UPRNHOOK2  # UPRN retrieval
+yottadb -run ^NEL             # request handling
+yottadb -run ^ADDWEBAUTH      # Add creds
 
 # Drop in to ydb
 exec $ydb_dist/yottadb -direct
