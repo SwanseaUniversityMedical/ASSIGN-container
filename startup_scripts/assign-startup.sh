94d109077fc31edf83b794f5c02485cf2f738583
Mike Edwards <michael.edwards@swansea.ac.uk>
2024-11-20 17:26:55 +0000

    fix: Allow database rundown to restore after container stop

--- startup_scripts/assign-startup.sh
+++ startup_scripts/assign-startup.sh
@@ -2,12 +2,25 @@
 
 mkdir -p /data/logs
 
-# Set env vars
-. /opt/yottadb/current/ydb_env_set
+# Check if database file exists, if so try a rundown incase it was borked by container stopping
+if [ -f "$ydb_dir/$ydb_rel/g/yottadb.gld" ]; then
+  echo "Running rundown to restore database."
+  export ydb_gbldir="$ydb_dir/$ydb_rel/g/yottadb.gld"
+  /opt/yottadb/current/mupip rundown -region DEFAULT
+fi
 
-# Get ASSIGN
+# Check if env vars need setting (not ideal, but better than checking every one
+if [ -z "${ydb_dist}" ]; then
+  echo "ydb_dist not set. Setting variables."
+  . /opt/yottadb/current/ydb_env_set
+else
+  echo "ydb_dist already set. Not setting variables again."
+fi
+
+# Check if ASSIGN needs pulling
 if [ ! -d "$assign_dest" ]; then
   echo "Obtaining ASSIGN routines."
+
   # Clone the wanted sha from github
   git clone $assign_url $assign_dest
   git -C $assign_dest checkout $assign_sha
@@ -16,32 +29,36 @@ if [ ! -d "$assign_dest" ]; then
   echo "Moving the routines."
   cp $assign_dest/UPRN/yottadb/* $ydb_dir/$ydb_rel/r
   cp $assign_dest/UPRN/codelists/* $abp_dir
-
-  # Perform zlink of routines
-  echo "Linking ASSIGN routines."
-  $ydb_dist/ydb -run ^ZLINK
 else
   echo "$assign_dest already exists. Not cloning down ASSIGN again."
 fi
 
+# Perform zlink of routines, doesn't matter if already linked
+echo "Linking ASSIGN routines, you may see warnings."
+$ydb_dist/ydb -run ^ZLINK
+echo "Routines linked."
+
 # Update YottaDB database settings
 $ydb_dist/mupip set -NULL_SUBSCRIPTS=true -region DEFAULT && \
-$ydb_dist/mupip set -journal=off -region DEFAULT && \
 $ydb_dist/mupip set -extension_count=500000 -region DEFAULT && \
-$ydb_dist/mupip set -access_method=mm -region DEFAULT
+$ydb_dist/mupip set -journal=off -region DEFAULT #&& \
+#$ydb_dist/mupip set -access_method=mm -region DEFAULT
 
 # Set the ybd env var for the TLS password hash, this can be replaced with an ENV var in the image?
 export ydb_tls_passwd_dev="$($ydb_dist/plugin/ydbcrypt/maskpass <<< $cert_pass | cut -d ":" -f2 | tr -d ' ')"
 
 # Startup the ASSIGN TLS listener
+echo "Starting TLS listener"
 yottadb -run %XCMD 'job START^VPRJREQ(9081)' &
 
 # Startup the ASSIGN web API interface
-mv /data/ADDWEBAUTH.m $ydb_dir/$ydb_rel/r/ADDWEBAUTH.m
+echo "Starting ASSIGN API endpoints"
+cp "/tmp/ADDWEBAUTH.m" "$ydb_dir/$ydb_rel/r/ADDWEBAUTH.m"
 yottadb -run INT^VUE          # File upload/download
 yottadb -run SETUP^UPRNHOOK2  # UPRN retrieval
 yottadb -run ^NEL             # request handling
 yottadb -run ^ADDWEBAUTH      # Add creds
 
 # Startup webgui
+echo "Starting YottaDB GUI endpoint"
 yottadb -run %ydbgui --readwrite --port 9080 >>/data/logs/%ydbgui.log
